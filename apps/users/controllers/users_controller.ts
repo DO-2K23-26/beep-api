f5b89135b032ddc939288b399b8417b4c66fd84a
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2024-03-17 15:38:08 +0100

    fix: update users_controller

--- apps/users/controllers/users_controller.ts
+++ apps/users/controllers/users_controller.ts
@@ -3,8 +3,8 @@ import { inject } from '@adonisjs/core'
 import UserService from '#apps/users/services/user_service'
 import UserPolicy from '#apps/users/policies/user_policy'
 import AuthenticationService from '#apps/authentication/services/authentication_service'
-import User from '../models/user.js'
 import redis from '@adonisjs/redis/services/main'
+import transmit from "@adonisjs/transmit/services/main";
 
 @inject()
 export default class UsersController {
@@ -19,46 +19,39 @@ export default class UsersController {
 
   async connectUser({ response, auth }: HttpContext) {
     const payload = auth.use('jwt').payload
-    if (!(payload && typeof payload.sub === 'string')) {
-      return { error: 'User not found' }
-    }
 
-    const user = await User.query()
-      .where('id', payload.sub as string)
-      .firstOrFail()
+    await redis.hset('userStates', payload.sub, JSON.stringify({
+      id: payload.sub,
+      username: payload.username
+    }));
 
-    await redis.hset('userStates', user.id, JSON.stringify({ userId: user.id, userName: user.username }));
+    transmit.broadcast('users/state', {
+      message: 'new user connected'
+    })
 
-    return response.status(200).send({ message: 'User connected' });
-  };
+    return response.send({
+      message: 'User connected'
+    })
+  }
 
   async disconnectUser({ response, auth }: HttpContext) {
     const payload = auth.use('jwt').payload
-    if (!(payload && typeof payload.sub === 'string')) {
-      return { error: 'User not found' }
-    }
 
-    const user = await User.query()
-      .where('id', payload.sub as string)
-      .firstOrFail()
+    await redis.hdel('userStates', payload.sub)
 
-    await redis.hdel('userStates', user.id);
+    transmit.broadcast('users/state', {
+      message: 'new user disconnected'
+    })
 
-    return response.status(200).send({ message: 'User disconnected' });
-  };
+    return response.send({
+      message: 'User disconnected'
+    })
+  }
 
   async onlines({ response }: HttpContext) {
     const userStates = await redis.hgetall('userStates');
     const users = Object.values(userStates).map((userState) => JSON.parse(userState));
 
-    return response.send(users);
+    return response.send(users)
   }
-
-
-  // async register({ request, response }: HttpContext) {
-  //   const data = request.only(['email', 'password'])
-  //   const user = await this.userService.create(data)
-
-  //   return response.send(user)
-  // }
 }
