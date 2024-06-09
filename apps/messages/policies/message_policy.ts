3e649c636a2805def862987a1522afb4e84fd919
pauline <pauline.contat00@gmail.com>
2024-06-09 12:39:58 +0200

    add authorization to delete message as a server admin

--- apps/messages/policies/message_policy.ts
+++ apps/messages/policies/message_policy.ts
@@ -6,10 +6,16 @@ import { AuthorizerResponse } from '@adonisjs/bouncer/types'
 import { inject } from '@adonisjs/core'
 import { HttpContext } from '@adonisjs/core/http'
 import { JwtPayload } from 'jsonwebtoken'
+import ServerService from '#apps/servers/services/server_service'
+import ChannelService from '#apps/channels/services/channel_service'
+import { ShowChannelSchema } from '#apps/channels/validators/channel'
+import Channel from '#apps/channels/models/channel'
 
 @inject()
 export default class MessagePolicy extends BasePolicy {
   protected payload: JwtPayload
+  private serverService: ServerService
+  private channelService: ChannelService
 
   constructor(
     protected permissionResolver: PermissionResolver,
@@ -17,6 +23,8 @@ export default class MessagePolicy extends BasePolicy {
   ) {
     super()
     this.payload = ctx.auth.use('jwt').payload! as JwtPayload
+    this.serverService = new ServerService()
+    this.channelService = new ChannelService()
   }
 
   @allowGuest()
@@ -26,6 +34,18 @@ export default class MessagePolicy extends BasePolicy {
 
   @allowGuest()
   async delete(_user: User | null, message: Message): Promise<AuthorizerResponse> {
-    return message.ownerId === this.payload.sub
+    let isServerAdmin: boolean
+    let showChannelSchema: ShowChannelSchema = {
+      params: {
+        id: message.channelId,
+      },
+      users: true,
+      messages: undefined,
+    }
+    const channel: Channel = await this.channelService.findById(showChannelSchema)
+    const server = await this.serverService.findById(channel.serverId)
+    isServerAdmin = server.ownerId === this.payload.sub
+
+    return message.ownerId === this.payload.sub || isServerAdmin
   }
 }
