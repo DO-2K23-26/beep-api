aecb7a7688455d5a3088337a56fa218e405b280b
Théo Tchilinguirian <theo.tchlx@gmail.com>
2024-06-16 16:51:08 +0200

    feat(channels validator): translations, added update validator

--- apps/channels/validators/channel.ts
+++ apps/channels/validators/channel.ts
@@ -1,5 +1,6 @@
 import vine from '@vinejs/vine'
 import { Infer } from '@vinejs/vine/types'
+import { describe } from 'node:test'
 
 /**
  * Validator to validate the payload when creating
@@ -7,7 +8,7 @@ import { Infer } from '@vinejs/vine/types'
  */
 export const createChannelValidator = vine.compile(
   vine.object({
-    name: vine.string().minLength(1), // Assurez-vous que le nom est une chaîne non vide et obligatoire
+    name: vine.string().minLength(1), // name is not empty and must be given
     type: vine.string().in(['text', 'voice']),
   })
 )
@@ -15,12 +16,13 @@ export const createChannelValidator = vine.compile(
 /**
  * Validator to validate the payload when updating
  * an existing channel.
- * Généralement, pour une mise à jour, les champs ne sont pas tous obligatoires.
+ * Typically, for an update, not all fields are required.
  */
 export const updateChannelValidator = vine.compile(
   vine.object({
-    id: vine.string().uuid({ version: [4] }),
-    name: vine.string().minLength(1).optional(), // Le nom est facultatif pour la mise à jour, mais s'il est fourni, il doit être non vide
+    name: vine.string().minLength(1).optional(), // The name is optional for the update, but if provided, it must be non-empty
+    description: vine.string().minLength(1).optional(), // Same principle applies to the description
+    // It is not permitted/possible to update the type of a channel ; its id, its serverId, etc.
   })
 )
 
