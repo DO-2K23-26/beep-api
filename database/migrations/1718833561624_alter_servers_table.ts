6277bc74883de30fcd9ef86e6f4dd65b2e3b620c
Dorian Grasset <dorian.grasset.contact@gmail.com>
2024-06-25 21:42:17 +0200

    set migration default to private

--- database/migrations/1718833561624_alter_servers_table.ts
+++ database/migrations/1718833561624_alter_servers_table.ts
@@ -5,7 +5,7 @@ export default class extends BaseSchema {
 
   async up() {
     this.schema.alterTable(this.tableName, (table) => {
-      table.enum('visibility', ['public', 'private']).defaultTo('public')
+      table.enum('visibility', ['public', 'private']).defaultTo('private')
     })
   }
 }
