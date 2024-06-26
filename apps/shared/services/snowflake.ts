1746c889731d3768f20efb517b32df7adb79fb2f
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-06-26 11:44:54 +0200

    feat: optimize snowflake number generation
    This commit optimizes the `generateSnowflake` function in the `snowflake.ts` file. It removes unnecessary console.log statements and refactors the code to improve performance and readability.
    
    Co-authored-by: Louis Labeyrie <labeyrielouis@gmail.com>

--- apps/shared/services/snowflake.ts
+++ apps/shared/services/snowflake.ts
@@ -7,17 +7,10 @@ export function generateNumber(n: number): number {
 
 export function generateSnowflake(): string {
   const timestamp = Date.now()
-  // remove the first 3 digits of the timestamp
-  // and keep the last 9 digits
 
   const timestampStr = timestamp.toString().slice(7)
-  console.log('TimestampStr:', timestampStr)
-  console.log('Timestamp:', timestamp)
-
   const machineId = generateNumber(5)
   const sequence = generateNumber(4)
-
-  console.log('MachineId:', machineId)
 
   return `${machineId}${timestampStr}${sequence}`
 }
