b177907dbdf290739ea780859f60aaceaa881d3a
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2024-06-28 10:25:53 +0200

    deplace /users/:userId at the bottom

--- apps/users/routes.ts
+++ apps/users/routes.ts
@@ -5,19 +5,11 @@ const UsersController = () => import("#apps/users/controllers/users_controller")
 
 router.group(() => {
   router.get('', [UsersController, 'index'])
-  router.get('/:userId', [UsersController, 'show'])
   router.post('/connect', [UsersController, 'connectUser'])
   router.post('/disconnect', [UsersController, 'disconnectUser'])
   router.get('/onlines', [UsersController, 'onlines'])
   router.get('/display', [UsersController, 'all'])
-  router.group(() => {
-    router.group(() => {
-      router.post('', [UsersController, 'createEmailToken'])
-      router.put('', [UsersController, 'confirmEmailUpdate'])
-    }).prefix('/email')
-    router.get('',[UsersController, 'findMe'])
-    router.put('', [UsersController, 'update'])
-  }).prefix('/@me')
+  router.get('/:userId', [UsersController, 'show'])
 }).prefix('/users')
   .use(middleware.auth())
 
