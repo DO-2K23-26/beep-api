4b05712769f497c99f7d7e35af37452ce043335a
pauline.contat01 <pauline.contat01@etu.umontpellier.fr>
2024-11-14 08:35:50 +0000

    Resolve "[Notifications] Create an endpoint for triggering notifications"

--- start/routes.ts
+++ start/routes.ts
@@ -11,6 +11,8 @@ import router from '@adonisjs/core/services/router'
 import transmit from '@adonisjs/transmit/services/main'
 import swagger from '#config/swagger'
 import AutoSwagger from 'adonis-autoswagger'
+import { HttpContext } from '@adonisjs/core/http'
+import { JwtPayload } from 'jsonwebtoken'
 
 // returns swagger in YAML
 router.get('/swagger', async () => {
@@ -22,3 +24,7 @@ router.get('/docs', async () => {
 })
 
 transmit.registerRoutes()
+
+transmit.authorize<{ id: string }>('notifications/users/:id', (ctx: HttpContext, { id }) => {
+  return (ctx.auth.user as JwtPayload).sub === id
+})
