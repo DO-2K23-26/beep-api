d3bc91cd7aab96cbe1b690ebb33f4e556f056a7e
Théo Tchilinguirian <theo.tchlx@gmail.com>
2024-11-21 18:45:43 +0100

    feat(roles): api routes and module rename

--- apps/role/controllers/roles_controller.ts
+++
@@ -1,6 +0,0 @@
-import { inject } from '@adonisjs/core'
-import RoleService from '#apps/role/services/role_service'
-@inject()
-export default class RolesController {
-  constructor(protected roleService: RoleService) {}
-}
---
+++ apps/roles/services/role_service.ts
@@ -0,0 +1,48 @@
+import RoleNotFoundException from '#apps/roles/exceptions/role_not_found_exception'
+import Role from '#apps/roles/models/role'
+import { CreateRoleSchema, ShowRoleSchema, UpdateRoleSchema } from '#apps/roles/validators/role'
+import ServerNotFoundException from '#apps/servers/exceptions/server_not_found_exception'
+import Server from '#apps/servers/models/server'
+import { inject } from '@adonisjs/core'
+
+@inject()
+export default class RoleService {
+  async findById(data: ShowRoleSchema): Promise<Role> {
+    return Role.query().where('id', data.params.id).firstOrFail()
+  }
+
+  async findAllByServer(serverId: string): Promise<Role[]> {
+    const server = await Server.findOrFail(serverId).catch(() => {
+      throw new ServerNotFoundException('Server not found', { status: 404, code: 'E_ROWNOTFOUND' })
+    })
+    await server.load('roles')
+    return server.roles
+  }
+
+  async create(newRole: CreateRoleSchema, serverId: string): Promise<Role> {
+    const permissions = newRole.permissions
+    const role = await Role.create({
+      name: newRole.name,
+      permissions: permissions,
+      serverId: serverId,
+    })
+    return role.save()
+  }
+
+  async update(id: string, payload: UpdateRoleSchema): Promise<Role> {
+    const role = await Role.findOrFail(id).catch(() => {
+      throw new RoleNotFoundException('Role not found', {
+        status: 404,
+        code: 'E_ROWNOTFOUND',
+      })
+    })
+    role.merge(payload)
+
+    return role.save()
+  }
+
+  async deleteById(roleId: string): Promise<void> {
+    const role: Role = await Role.findOrFail(roleId)
+    await role.delete()
+  }
+}
