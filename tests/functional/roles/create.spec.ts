686d8dab705e6a4fee9bdbe7236bec55e074330c
Théo Tchilinguirian <theo.tchlx@gmail.com>
2024-12-09 17:00:23 +0100

    feat(roles): update create test

--- tests/functional/roles/create.spec.ts
+++ tests/functional/roles/create.spec.ts
@@ -3,14 +3,14 @@ import { ServerFactory } from '#database/factories/server_factory'
 import { test } from '@japa/runner'
 
 test.group('Roles create', () => {
-  test('example test', async ({ client }) => {
+  test('must return a 201 when creating a role', async ({ client }) => {
     const server = await ServerFactory.with('members').create()
     await server.load('members')
 
     const member = server.members[0]
     await member.load('user')
     const payload = {
-      name: 'Admin',
+      name: 'My role',
       permissions: Permissions.MANAGE_MESSAGES,
     }
     const result = await client
