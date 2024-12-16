1c8649bb3404e8d3aeed47c77b8ecdba9a05a782
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2024-12-16 20:00:18 +0100

    feat(roles): enhance role management tests and policy checks

--- tests/functional/roles/delete.spec.ts
+++ tests/functional/roles/delete.spec.ts
@@ -4,11 +4,46 @@ import { MemberFromFactory } from '#database/factories/member_factory'
 import { UserFactory } from '#database/factories/user_factory'
 
 test.group('Roles delete', () => {
-  test('must return 200 when deleted successfully', async ({ client, assert }) => {
+  test('must return 200 when deleted successfully', async ({ client }) => {
     const role = await RoleFactory.create()
     const user = await UserFactory.create()
     await MemberFromFactory(role.serverId, user.id).create()
     const response = await client.delete(`/servers/${role.serverId}/roles/${role.id}`).loginAs(user)
-    assert.equal(response.status(), 200)
-  })
+    response.assertStatus(200)
+  }).tags(['roles:destroy'])
+
+  test('must return 401 if your are not login', async ({ client }) => {
+    const role = await RoleFactory.create()
+    const user = await UserFactory.create()
+    await MemberFromFactory(role.serverId, user.id).create()
+    const response = await client.delete(`/servers/${role.serverId}/roles/${role.id}`)
+    response.assertStatus(401)
+  }).tags(['roles:destroy'])
+
+  test('must return 403 when user is not a member of the server', async ({ client }) => {
+    const role = await RoleFactory.create()
+    const user = await UserFactory.create()
+    const response = await client.delete(`/servers/${role.serverId}/roles/${role.id}`).loginAs(user)
+    response.assertStatus(403)
+  }).tags(['roles:destroy'])
+
+  test('must return 404 if the server does not exist', async ({ client }) => {
+    const role = await RoleFactory.create()
+    const user = await UserFactory.create()
+    await MemberFromFactory(role.serverId, user.id).create()
+    const response = await client
+      .delete(`/servers/nonexistantServerId/roles/${role.id}`)
+      .loginAs(user)
+    response.assertStatus(404)
+  }).tags(['roles:destroy'])
+
+  test('must return 404 if the role does not exist', async ({ client }) => {
+    const role = await RoleFactory.create()
+    const user = await UserFactory.create()
+    await MemberFromFactory(role.serverId, user.id).create()
+    const response = await client
+      .delete(`/servers/${role.serverId}/roles/nonexistantRoleId`)
+      .loginAs(user)
+    response.assertStatus(404)
+  }).tags(['roles:destroy'])
 })
--- tests/functional/roles/update.spec.ts
+++ tests/functional/roles/update.spec.ts
@@ -13,8 +13,55 @@ test.group('Roles update', () => {
       .put(`/servers/${role.serverId}/roles/${role.id}`)
       .json(data)
       .loginAs(user)
-    assert.equal(response.status(), 200)
+    response.assertStatus(200)
     assert.equal(response.body().name, data.name)
     assert.equal(response.body().permissions, data.permissions)
-  })
+  }).tags(['roles:update'])
+
+  test('must return 401 if your are not login', async ({ client }) => {
+    const role = await RoleFactory.create()
+    const data = { name: 'new name', permissions: 888 }
+    const response = await client.put(`/servers/${role.serverId}/roles/${role.id}`).json(data)
+    response.assertStatus(401)
+  }).tags(['roles:update'])
+
+  test('must return 403 when user is not a member of the server', async ({ client }) => {
+    const user = await UserFactory.make()
+    const role = await RoleFactory.create()
+    const data = { name: 'new name', permissions: 888 }
+
+    const result = await client
+      .put(`/servers/${role.serverId}/roles/${role.id}`)
+      .json(data)
+      .loginAs(user)
+    result.assertStatus(403)
+  }).tags(['roles:update'])
+
+  test('must return 404 if the server does not exist', async ({ client }) => {
+    const role = await RoleFactory.create()
+    const user = await UserFactory.create()
+    await MemberFromFactory(role.serverId, user.id).create()
+    const data = { name: 'new name', permissions: 888 }
+
+    const response = await client
+      .put(`/servers/nonexistantServerId/roles/${role.id}`)
+      .json(data)
+      .loginAs(user)
+
+    response.assertStatus(404)
+  }).tags(['roles:update'])
+
+  test('must return 404 if the role does not exist', async ({ client }) => {
+    const role = await RoleFactory.create()
+    const user = await UserFactory.create()
+    await MemberFromFactory(role.serverId, user.id).create()
+    const data = { name: 'new name', permissions: 888 }
+
+    const response = await client
+      .put(`/servers/${role.serverId}/roles/nonexistantRoleId`)
+      .json(data)
+      .loginAs(user)
+
+    response.assertStatus(404)
+  }).tags(['roles:update'])
 })
