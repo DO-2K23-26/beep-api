64667f435d081920c609ab90e8b5d5f7fb66893f
Giada De Martino <giada-de-martino@etu.umontpellier.fr>
2025-01-16 15:32:12 +0100

    prettier fix

--- apps/authentication/controllers/authentication_controller.ts
+++ apps/authentication/controllers/authentication_controller.ts
@@ -24,6 +24,7 @@ import { Payload } from '../contracts/payload.js'
 import { SignIn } from '../contracts/signin.js'
 import transmit from '@adonisjs/transmit/services/main'
 import logger from '@adonisjs/core/services/logger'
+import PasswordMismatchException from '#apps/authentication/exceptions/password_mismatch_exception'
 
 @inject()
 export default class AuthenticationController {
@@ -156,7 +157,15 @@ export default class AuthenticationController {
     const validator = await request.validateUsing(updatePasswordValidator)
     const payload = auth.use('jwt').payload!
 
-    await this.authenticationService.updateNewPassword(payload.email, validator)
+    await this.authenticationService.updateNewPassword(payload.email, validator).catch(() => {
+      throw new PasswordMismatchException(
+        'The inserterd password is not matching the current one.',
+        {
+          status: 400,
+          code: 'E_PASSWORDMISMATCHING',
+        }
+      )
+    })
 
     return response.send({ message: 'Password updated successfully.' })
   }
