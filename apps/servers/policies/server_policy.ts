1b9d277be8b943fd4de16bdceb2fb691ad135bc4
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2025-01-29 22:01:01 +0100

    feat:permission servers

--- apps/servers/policies/server_policy.ts
+++ apps/servers/policies/server_policy.ts
@@ -1,18 +1,41 @@
-import Server from '#apps/servers/models/server'
 import { BasePolicy } from '@adonisjs/bouncer'
-import { AuthorizerResponse } from '@adonisjs/bouncer/types'
 import { inject } from '@adonisjs/core'
 import { JwtPayload } from 'jsonwebtoken'
+import ServerService from '../services/server_service.js'
+import RoleService from '#apps/roles/services/role_service'
+import PermissionsService from '#apps/shared/services/permissions/permissions_service'
+import { Permissions } from '#apps/shared/enums/permissions'
 
 @inject()
 export default class ServerPolicy extends BasePolicy {
-  before() {}
+  constructor(
+    protected serverService: ServerService,
+    protected roleService: RoleService,
+    protected permissionsService: PermissionsService
+  ) {
+    super()
+  }
+  async before(payload: JwtPayload, _action: string, ...params: never[]) {
+    const serverId: string | undefined = params[0]
+    const isPresent = await this.serverService.userPartOfServer(payload.sub!, serverId!)
+    if (!isPresent) return false
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  async view(_payload: JwtPayload, _server: Server): Promise<AuthorizerResponse> {
+    const permissions = await this.roleService.getMemberPermissions(payload.sub!, serverId!)
+    const isAdministrator = this.permissionsService.has_permission(
+      permissions,
+      Permissions.ADMINISTRATOR
+    )
+    if (isAdministrator) return true
+  }
+  async view() {
     return true
   }
-  async edit(payload: JwtPayload, server: Server): Promise<AuthorizerResponse> {
-    return server.ownerId === payload.sub
+  async edit(payload: JwtPayload, serverId: string) {
+    const permissions = await this.roleService.getMemberPermissions(payload.sub!, serverId)
+    return this.permissionsService.has_permission(permissions, Permissions.MANAGE_SERVER)
+  }
+  async delete(payload: JwtPayload, serverId: string) {
+    const permissions = await this.roleService.getMemberPermissions(payload.sub!, serverId)
+    return this.permissionsService.has_permission(permissions, Permissions.MANAGE_SERVER)
   }
 }
