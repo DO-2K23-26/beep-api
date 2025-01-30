197a73e09a775c28162d19e8c1d429b3e0d56756
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2025-01-30 19:10:24 +0100

    fix(test):role

--- tests/functional/invitations/servers/create.spec.ts
+++ tests/functional/invitations/servers/create.spec.ts
@@ -1,5 +1,7 @@
-import { InvitationStatus } from '#apps/invitations/models/status'
 import { InvitationType } from '#apps/invitations/models/type'
+import { Permissions } from '#apps/shared/enums/permissions'
+import { MemberFactory } from '#database/factories/member_factory'
+import { RoleFactory } from '#database/factories/role_factory'
 import { ServerFactory } from '#database/factories/server_factory'
 import { UserFactory } from '#database/factories/user_factory'
 import { test } from '@japa/runner'
@@ -7,6 +9,13 @@ import { test } from '@japa/runner'
 test.group('Invitations server create ', () => {
   test('must return 201 when creating unique', async ({ client, expect, assert }) => {
     const server = await ServerFactory.merge({ visibility: 'private' }).with('members').create()
+    const member = await MemberFactory.merge({ serverId: server.id }).create()
+    const role = await RoleFactory.merge({
+      permissions: Permissions.CREATE_INVITATION,
+      serverId: server.id,
+    }).create()
+    await role.related('members').attach([member.id])
+    await member.load('user')
     await server.load('members', async (query) => {
       await query.preload('user')
     })
@@ -16,18 +25,24 @@ test.group('Invitations server create ', () => {
         expiration: '2030-01-01T00:00:00.000Z',
         isUnique: true,
       })
-      .loginAs(server.members[0].user)
+      .loginAs(member.user)
     response.assertStatus(201)
     expect(response.body()).toHaveProperty('expiration')
     assert.containsSubset(response.body(), {
-      creatorId: server.members[0].user.id,
+      creatorId: member.user.id,
       serverId: server.id,
       type: InvitationType.SERVER,
-      status: InvitationStatus.Pending,
     })
   }).tags(['invitations:create'])
   test('must return 201 when creating not unique', async ({ client, expect, assert }) => {
     const server = await ServerFactory.merge({ visibility: 'private' }).with('members').create()
+    const member = await MemberFactory.merge({ serverId: server.id }).create()
+    const role = await RoleFactory.merge({
+      permissions: Permissions.CREATE_INVITATION,
+      serverId: server.id,
+    }).create()
+    await role.related('members').attach([member.id])
+    await member.load('user')
     await server.load('members', async (query) => {
       await query.preload('user')
     })
@@ -37,17 +52,24 @@ test.group('Invitations server create ', () => {
         expiration: '2030-01-01T00:00:00.000Z',
         isUnique: false,
       })
-      .loginAs(server.members[0].user)
+      .loginAs(member.user)
     response.assertStatus(201)
     expect(response.body()).toHaveProperty('expiration')
     assert.containsSubset(response.body(), {
-      creatorId: server.members[0].user.id,
+      creatorId: member.user.id,
       serverId: server.id,
       type: InvitationType.SERVER,
     })
   }).tags(['invitations:create'])
   test('must return 400 when creating invitation for a public server', async ({ client }) => {
     const server = await ServerFactory.merge({ visibility: 'public' }).with('members').create()
+    const member = await MemberFactory.merge({ serverId: server.id }).create()
+    const role = await RoleFactory.merge({
+      permissions: Permissions.CREATE_INVITATION,
+      serverId: server.id,
+    }).create()
+    await role.related('members').attach([member.id])
+    await member.load('user')
     await server.load('members', async (query) => {
       await query.preload('user')
     })
@@ -57,7 +79,7 @@ test.group('Invitations server create ', () => {
         expiration: '2030-01-01T00:00:00.000Z',
         isUnique: true,
       })
-      .loginAs(server.members[0].user)
+      .loginAs(member.user)
     response.assertStatus(400)
   }).tags(['invitations:create'])
 
